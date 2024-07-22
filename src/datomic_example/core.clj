17d1e42a11f589e147e20b80b7bc72dee9565cfd
Josue santos <josue.santos.dev@gmail.com>
2024-07-22 14:06:27 -0300

    datomic exemplo

--- src/datomic_example/core.clj
+++ src/datomic_example/core.clj
@@ -1,6 +1,43 @@
-(ns datomic-example.core)
+(ns datomic-example.core
+  (:require [datomic.api :as d]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; datomic uri in-memory
+(def uri "datomic:mem://datomic-example")
+
+;; cria o banco
+(d/create-database uri)
+
+;; connect no banco
+(def datomic (d/connect uri))
+
+;; cria um schema simples
+(def schema
+  [{:db/ident       :person/name
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "A person's name"}
+   {:db/ident       :person/email
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity
+    :db/doc         "A person's email"}
+   {:db/ident       :person/password
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "A person's password"}])
+
+;; grava o schema no banco
+(d/transact datomic schema)
+
+;; um exemplo de entidade person
+(def person
+  [{:person/name "Josue"
+    :person/email "josue@test.mail"
+    :person/password "senhasupersecreta123"}])
+
+;; grava person no banco
+(d/transact datomic person)
+
+;; faz uma busca no banco
+(d/pull (d/db datomic) '[*] [:person/email "josue@test.mail"])
+
